5e7ce742b692300c31fc95a7e68059a83a864e68
HiepHT411 <hoangtuanhiep.04112000@gmail.com>
2022-02-27 16:56:56 +0700

    init project for server side

---
+++ src/main/java/com/hoanghiep/perfume/HustperfumeServerApplication.java
@@ -0,0 +1,13 @@
+package com.hoanghiep.perfume;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HustperfumeServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HustperfumeServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hoanghiep/perfume/HustperfumeServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hoanghiep.perfume;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HustperfumeServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
